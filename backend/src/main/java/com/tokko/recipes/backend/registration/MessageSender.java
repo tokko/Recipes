02fd79e45f88d7db1ecda45f78c65d5249639e2d
tokko <andreg@kth.se>
2015-06-14 20:01:58 +0200

    updating registrationIds

--- backend/src/main/java/com/tokko/recipes/backend/registration/MessageSender.java
+++ backend/src/main/java/com/tokko/recipes/backend/registration/MessageSender.java
@@ -1,6 +1,7 @@
 package com.tokko.recipes.backend.registration;
 
 import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Result;
 import com.google.android.gcm.server.Sender;
 import com.google.appengine.api.users.User;
 import com.google.appengine.repackaged.com.google.gson.Gson;
@@ -16,10 +17,16 @@ public class MessageSender {
     public static <T> void sendMessage(T entity, User user) {
         Sender sender = new Sender(API_KEY);
         Message msg = new Message.Builder().addData("message", new Gson().toJson(entity)).build();
-        List<Registration> records = ofy().load().type(Registration.class).list(); //filter("userId=", user.getEmail()).list(); //.filter("userId=", user.getUserEmail()).list();
+        List<Registration> records = ofy().load().type(Registration.class).filter("userEmail=", user.getEmail()).list();
         try {
             for (Registration record : records) {
-                sender.send(msg, record.getRegId(), 5);
+                Result res = sender.send(msg, record.getRegId(), 5);
+                if (res.getCanonicalRegistrationId() == null)
+                    ofy().delete().entity(record).now();
+                else if (!record.getRegId().equals(res.getCanonicalRegistrationId())) {
+                    record.setRegId(res.getCanonicalRegistrationId());
+                    ofy().save().entity(record).now();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
