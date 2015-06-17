b1b466944cf260f5f90a450d0c65e3182bb51d9d
Andreas Gustafsson <andreas.gustafsson@beamonpeople.se>
2015-06-17 16:38:56 +0200

    more test (unfinished), maybe use backport streams and retrolambda?

--- backend/src/test/java/com/tokko/recipes/backend/resourceaccess/RecipeRaTests.java
+++ backend/src/test/java/com/tokko/recipes/backend/resourceaccess/RecipeRaTests.java
@@ -7,6 +7,7 @@ import com.tokko.recipes.backend.util.TestsWithObjectifyStorage;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -16,11 +17,13 @@ public class RecipeRaTests extends TestsWithObjectifyStorage {
 
 
     private RecipeRa recipeRa;
+    private RegistrationRA registrationRa;
 
     @Before
     public void setup(){
         super.setup();
         recipeRa = new RecipeRa(ofy);
+        registrationRa = new RegistrationRA(ofy);
     }
 
     @Test
@@ -37,5 +40,18 @@ public class RecipeRaTests extends TestsWithObjectifyStorage {
         List<Recipe> recipes = recipeRa.getRecipesForUser(new RecipeUser("56436", 1L));
         assertNotNull(recipes);
         assertEquals(0, recipes.size());
+    }
+
+    @Test
+    public void getRecipesForUse_OnlyUsersRecipesAreFetched(){
+        final RecipeUser user1 = new RecipeUser("user1");
+        RecipeUser user2 = new RecipeUser("user2");
+
+        registrationRa.saveUser(user1);
+        registrationRa.saveUser(user2);
+
+        List<Recipe> recipes1 = Arrays.asList(new Recipe("recipe1"), new Recipe("recipe2"));
+        List<Recipe> recipes2 = Arrays.asList(new Recipe("recipe2"), new Recipe("recipe2"));
+        //TODO: finish this test
     }
 }
